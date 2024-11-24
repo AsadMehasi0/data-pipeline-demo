1094a82f0575e9f1816a10b6e89a17eedcd3f849
Asad Mehasi <94223379+AsadMehasi0@users.noreply.github.com>
2024-11-24 13:29:54 +0000

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -2,7 +2,7 @@
 # !pip install -r requirements.txt
 
 # %%
-import functions.py
+import functions as f
 import time
 import datetime
 
@@ -11,28 +11,28 @@ print("----------------------------------------------")
 
 # Step 1: extract video IDs
 t0 = time.time()
-getVideoIDs()
+f.getVideoIDs()
 t1 = time.time()
 print("Step 1: Done")
 print("---> Video IDs downloaded in", str(t1-t0), "seconds", "\n")
 
 # Step 2: extract transcripts for videos
 t0 = time.time()
-getVideoTranscripts()
+f.getVideoTranscripts()
 t1 = time.time()
 print("Step 2: Done")
 print("---> Transcripts downloaded in", str(t1-t0), "seconds", "\n")
 
 # Step 3: Transform data
 t0 = time.time()
-transformData()
+f.transformData()
 t1 = time.time()
 print("Step 3: Done")
 print("---> Data transformed in", str(t1-t0), "seconds", "\n")
 
 # Step 4: Generate text emebeddings
 t0 = time.time()
-createTextEmbeddings()
+f.createTextEmbeddings()
 t1 = time.time()
 print("Step 4: Done")
 print("---> Embeddings generated in", str(t1-t0), "seconds", "\n")
